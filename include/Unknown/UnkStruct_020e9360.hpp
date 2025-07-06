5e101a2c8d8c8162cc655dd270fa2a3003785ff8
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-06 20:23:31 +0200

    fix splits + new headers

---
+++ include/Unknown/UnkStruct_020e9360.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "global.h"
+#include "types.h"
+
+class UnkStruct_020e9360 {
+public:
+    /* 00 */
+
+    UnkStruct_020e9360();
+};
+
+extern UnkStruct_020e9360 data_ov000_020e9360;
---
+++ include/Unknown/UnkStruct_020ec7dc.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "global.h"
+#include "types.h"
+
+class UnkStruct_020ec7dc {
+public:
+    /* 00 */
+
+    UnkStruct_020ec7dc();
+};
+
+extern UnkStruct_020ec7dc data_ov000_020ec7dc;
