f8da663319c54244a2227f48d8582766ff2d80c9
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-07 12:36:19 +0200

    fix build issues

--- include/Unknown/UnkStruct_020ec7dc.hpp
+++ include/Unknown/UnkStruct_020ec7dc.hpp
@@ -3,6 +3,8 @@
 #include "global.h"
 #include "types.h"
 
+#include "Game/GameMode.hpp"
+
 class UnkStruct_020ec7dc_00 {
 public:
     /* 00 */ u16 mUnk_00[0xA];
@@ -18,6 +20,7 @@ public:
     UnkStruct_020ec7dc();
 
     void func_ov000_0207c170();
+    void func_ov000_0207c0f0(GameModeId modeId);
 };
 
 extern UnkStruct_020ec7dc data_ov000_020ec7dc;
