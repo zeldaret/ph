59e25cc63093a0b3e466b33e6af68be14e2e41bf
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-22 10:35:29 +0200

    split 7750 and 86d8

--- src/Main/Unknown/UnkStruct_02037750.cpp
+++ src/Main/Unknown/UnkStruct_02037750.cpp
@@ -9,6 +9,8 @@ extern "C" ARM unk32 func_02037628(u8 *);
 extern u8 *data_027e0d54;
 extern unk32 *data_027e0cb4;
 
+char *gShipParts[8] = {"brg", "anc", "pdl", "hul", "can", "dco", "bow", "fnl"};
+
 ARM UnkStruct_02037750::UnkStruct_02037750(unk32 param1, unk32 param2) {
     s32 i;
 
@@ -204,30 +206,3 @@ ARM bool UnkStruct_02037750::vfunc_1c() {
 ARM void UnkStruct_02037750::vfunc_20() {
     this->mUnk_18 = 1 << 24;
 }
-
-// non-matching
-ARM UnkStruct_020386d8::UnkStruct_020386d8() :
-    UnkStruct_02037750(1 << 24, 0) {
-    this->mUnk_20 = -1;
-    this->mUnk_24.func_020376c0(&gVec3p_ZERO);
-}
-
-ARM void UnkStruct_020386d8::vfunc_08(u32 param1) {
-    this->UnkStruct_02037750::vfunc_08(param1);
-    this->mUnk_20 = 0xff;
-}
-
-ARM bool UnkStruct_020386d8::vfunc_24(unk32 param1) {
-    return this->func_020385d0(param1, &this->mUnk_24.mUnk_00) != 0;
-}
-
-ARM unk32 UnkStruct_020386d8::vfunc_28(s32 param1) {
-    this->mUnk_20 = this->UnkStruct_02037750::vfunc_28(param1);
-    return this->mUnk_20;
-}
-
-ARM bool UnkStruct_020386d8::vfunc_30() {
-    return gMessageManager.func_020366c4()->mUnk_15c <= 0;
-}
-
-ARM UnkStruct_020386d8::~UnkStruct_020386d8() {}
