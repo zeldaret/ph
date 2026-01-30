98cc0af690719f10bd7b67c1c11d207bfcdbbfe7
Yanis <35189056+Yanis002@users.noreply.github.com>
2026-01-30 18:36:20 +0100

    Started  `TouchControl.cpp` (#111)
    * start both `TouchControl.cpp`
    
    * fix build issues
    
    * complete touchcontrol
    
    * remove useless comment
    
    ---------
    
    Co-authored-by: Yanis002 <Yanis002@users.noreply.github.com>

--- src/Main/Player/TouchControl.cpp
+++ src/Main/Player/TouchControl.cpp
@@ -1,4 +1,24 @@
 #include "Player/TouchControl.hpp"
+#include "global.h"
 
-bool TouchControl::func_0202b864(Vec3p *param1, s32 size, unk8 param3) {}
-bool TouchControl::func_0202b894(Vec3p *param1, s32 size, unk8 param3) {}
+extern "C" bool func_0202b8f8(Vec3p *, s32, s32, s32, unk8);
+
+ARM bool TouchControl::func_0202b864(Vec3p *param1, s32 size, unk8 param3) {
+    if (gTouchControl.mFlags & TouchFlag_TouchedNow) {
+        return func_0202b8f8(param1, size, gTouchControl.mTouchX, gTouchControl.mTouchY, param3);
+    }
+
+    return false;
+}
+
+ARM bool TouchControl::func_0202b894(Vec3p *param1, s32 size, unk8 param3) {
+    if (gTouchControl.mTouch) {
+        return func_0202b8f8(param1, size, gTouchControl.mTouchX, gTouchControl.mTouchY, param3);
+    }
+
+    if (gTouchControl.mFlags & TouchFlag_UntouchedNow) {
+        return func_0202b8f8(param1, size, gTouchControl.mTouchPrevX, gTouchControl.mTouchPrevY, param3);
+    }
+
+    return false;
+}
