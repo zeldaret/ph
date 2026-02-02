5a7cba4088236394810194b29232cd0f6c790780
enzo <enzocarnevali708@gmail.com>
2026-02-02 12:01:31 -0300

    rename boxmodelrender

--- include/Render/TreasureChestModelRender.hpp
+++ include/Render/TreasureChestModelRender.hpp
@@ -2,14 +2,14 @@
 
 #include "ModelRender.hpp"
 
-class BoxModelRender : public ModelRender {
+class TreasureChestModelRender : public ModelRender {
 public:
     /* 00 (base) */
     /* 5c */ bool isClosed;
     /* 5d */
 
-    /* 00 */ virtual ~BoxModelRender() override;
+    /* 00 */ virtual ~TreasureChestModelRender() override;
     /* 3c */ virtual void vfunc_3c(ModelRenderCommandsData *renderData) override;
 
-    BoxModelRender(ItemModel *itemModel);
+    TreasureChestModelRender(ItemModel *itemModel);
 };
