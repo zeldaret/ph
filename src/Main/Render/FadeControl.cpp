a7c25e1e8ff265c5eefb95fb24b16de3fd9259ef
Aurel <285191559+0Aurel@users.noreply.github.com>
2026-05-16 16:22:53 +0200

    Remove unnecessary FadeControl constructor definition

--- src/Main/Render/FadeControl.cpp
+++ src/Main/Render/FadeControl.cpp
@@ -2,7 +2,6 @@
 
 void FadeControl::Register() {}
 void FadeControl::Unregister() {}
-FadeControl::FadeControl() {}
 void FadeControl::func_0202aba8(s32 fadeLength, unk32 param2, unk32 newState) {}
 bool FadeControl::func_0202abdc(s32 fadeLength, unk32 param2) {}
 bool FadeControl::func_0202abf4(s32 fadeLength, unk32 param2) {}
